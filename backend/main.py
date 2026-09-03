fbd6beb3534ecf3407fddf542afa745f87aa9e6a
Vicky Vignesh <vigneshvicky543543@gmail.com>
2026-09-03 09:31:15 +0530

    fix backend import and update

--- backend/main.py
+++ backend/main.py
@@ -102,4 +102,4 @@ def build_state(body: dict):
     return {"cube_state": state}
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
